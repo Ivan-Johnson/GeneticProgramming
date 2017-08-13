f40af0b8dcc7885cc825c0d45b66c8e70324c01f
Ivan Johnson <IvanJohnson@ComputerScience.email>
2017-08-13 01:19:15 -0500

    Add ant breeding functionality

--- Makefile
+++ Makefile
@@ -4,8 +4,6 @@
 CFLAGS += -Wall -Wextra -Wfatal-errors -Werror -std=c99
 LDLIBS = -lm
 
-
-
 OPTS_DEBUG = -D DEBUG -O0 -ggdb -fno-inline
 OPTS_OPTIMIZED = -O3
 
@@ -14,7 +12,7 @@ CFLAGS += $(OPTS_DEBUG)
 
 
 
-all: test
+all: test main-Ant
 
 ant.o: ant.h
 
@@ -24,10 +22,15 @@ test.o: geneticAlgorithm.h helper.h
 
 test: test.o geneticAlgorithm.o helper.o
 
+main-Ant.o: MapGen/map.c ant.h helper.h world.h geneticAlgorithm.h
+
 world.o: world.h ant.h
 
 MapGen/map.c: MapGen/map.txt MapGen/convertMap.bash
 	./MapGen/convertMap.bash $< > $@
+
+main-Ant: main-Ant.o geneticAlgorithm.o helper.o ant.o world.o
+
 clean:
 	rm -f *.o test
 
---
+++ main-Ant.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+
+#include "helper.h"
+#include "geneticAlgorithm.h"
+#include "ant.h"
+#include "world.h"
+
+void* randAnt(){
+	Ant *a = malloc(sizeof(Ant));
+	a->state = 0;
+	for(int x = 0; x<ANTINPUT_COUNT; x++) {
+		for(int y = 0; y<STATE_COUNT; y++) {
+			a->stateTable[x][y] = rand() % STATE_COUNT;
+			a->actionTable[x][y] = rand() % ANTACTION_COUNT;
+		}
+	}
+	return a;
+}
+
+World w;
+static void initTheWorld(){
+	w.x = 0;
+	w.y = 0;
+	w.facing = south;
+	for(int x = 0; x<STATE_COUNT; x++) {
+		for(int y = 0; y<ANTINPUT_COUNT; y++) {
+			w.hasFood[x][y] = false;
+		}
+	}
+	#include "MapGen/map.c"
+}
+
+unsigned int getFitness(void *ant){
+	return WorldRun(w, ant, NULL);
+}
+
+void breedAnt(void *s1, void *s2){
+	Ant *a1 = (Ant *) s1;
+	Ant *a2 = (Ant *) s2;
+
+	int i = 1 + rand() % (STATE_COUNT - 2);
+	Ant tmp;
+
+	for(int x = 0; x < ANTINPUT_COUNT; x++){
+		memcpy(tmp.stateTable[x], a1->stateTable[x],
+			sizeof(unsigned int) * i);
+		memcpy(a1->stateTable[x], a2->stateTable[x],
+			sizeof(unsigned int) * i);
+		memcpy(a2->stateTable[x], tmp.stateTable[x],
+			sizeof(unsigned int) * i);
+
+		memcpy(tmp.actionTable[x], a1->actionTable[x],
+			sizeof(AntActs) * i);
+		memcpy(a1->actionTable[x], a2->actionTable[x],
+			sizeof(AntActs) * i);
+		memcpy(a2->actionTable[x], tmp.actionTable[x],
+			sizeof(AntActs) * i);
+	}
+}
+
+void mutateAnt(void *vp){
+	Ant *a = (Ant*) vp;
+	int iState = rand() % STATE_COUNT;
+	int iInput = rand() % ANTINPUT_COUNT;
+	if(rand() % 2){
+		//randomize a single entry in the state table
+		a->stateTable[iInput][iState] = rand() % STATE_COUNT;
+	} else {
+		//randomize a single entry in the action table
+		a->actionTable[iInput][iState] = rand() % ANTACTION_COUNT;
+	}
+}
+
+void* cloneAnt(void *asdf){
+	Ant *a = (Ant*) asdf;
+	Ant *b = AntClone(a);
+	a->state = 0;
+	b->state = 0;
+	return b;
+}
+
+int main (int argc, char **argv){
+	(void) argc;
+	(void) argv;
+
+	initTheWorld();
+
+	//*
+	srand(time(NULL));
+	//srand(0);
+
+	geneticParams p = geneticParamsDefault();
+	p.popSize = 32000;
+	p.genCount = 20;
+	//p.breedRatio = 0.5;
+	//p.mutateRatio = 0;
+
+	Ant *a = geneticAlgorithm(p, randAnt, getFitness, cloneAnt,
+				mutateAnt, breedAnt);
+
+	//WorldRun(w, a, stdout);
+	WorldRun(w, a, NULL);
+	//TODO: display ant, somehow
+	free(a);
+	//*/
+	return 0;
+}
